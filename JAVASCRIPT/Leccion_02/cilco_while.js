340c9453e9f06ca79587be9abb4e6b28c5c56d71
chrisfalla <christoferfallavillarreal@gmail.com>
2024-12-21 13:07:57 -0500

    ciclo for y break

---
+++ JAVASCRIPT/Leccion_02/cilco_while.js
@@ -0,0 +1,34 @@
+// ciclo while
+
+// let contador = 0;
+// while (contador < 21) {
+//     console.log(contador);
+//     contador++;
+// }
+// console.log("Fin del ciclo while");
+
+// ciclo do while
+// let contador = 0;
+// while (contador < 21) {
+//     console.log(contador);
+//     contador++;
+// }
+// do {
+//     console.log(contador);
+//     contador++;
+// } while (contador < 3);
+// console.log("Fin del ciclo Do while");
+
+// ciclo for 
+// for (let contador = 0; contador < 11; contador++) {
+//     console.log(contador)
+// };
+// console.log("fin del ciclo for");
+
+for (let count = 0; count <= 10; count++) {
+    if (count % 2 == 0) {
+        console.log(count)
+        break;
+    }
+}
+console.log("fin del ciclo for")
--- JAVASCRIPT/Leccion_02/senetencias_decontrol.js
+++ JAVASCRIPT/Leccion_02/senetencias_decontrol.js
@@ -89,3 +89,4 @@
 // console.log(estacion);
 
 
+// ciclo while 
