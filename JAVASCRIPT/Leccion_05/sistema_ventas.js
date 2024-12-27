73e77ff4991acc745c36c3dfcf90cfccfc792e75
chrisfalla <christoferfallavillarreal@gmail.com>
2024-12-27 00:07:13 -0500

    terminado el sistema de ventas con poo

--- JAVASCRIPT/Leccion_05/sistema_ventas.js
+++ JAVASCRIPT/Leccion_05/sistema_ventas.js
@@ -24,5 +24,54 @@ class Producto {
         return this.idProducto + " " + this.nombre + " " + this.precio;
     }
 }
-let producto1 = new Producto("chris", 3000);
-console.log(producto1.toString());
+
+class Orden {
+    static contadorOrdenes = 0;
+    static get MAX_PRODUCTOS() {
+        return 5;
+    }
+    constructor() {
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this.productosAgregados = 0;
+    }
+    get idOrden() {
+        return this._idOrden;
+    }
+    agregarProductos(producto) {
+        if (this._productos.length < Orden.MAX_PRODUCTOS) {
+            this._productos.push(producto);
+        }
+        else {
+            return console.log("no se pueden agregar mas productos");
+        }
+    }
+    calcularTotal() {
+        let totalVenta = 0;
+        for (let producto of this._productos) {
+            totalVenta += producto.precio;
+        }
+        return totalVenta;
+    }
+    mostrarOrden() {
+        let productosOrden = " ";
+        for (let producto of this._productos) {
+            productosOrden +=  producto.toString() + " ";
+        }
+        console.log(`Orden: ${this._idOrden} Total: ${this.calcularTotal()} Productos: ${productosOrden}`);
+    }
+}
+
+let producto1 = new Producto("casco", 200);
+let producto2 = new Producto("jean", 150);
+let orden1 = new Orden();
+orden1.agregarProductos(producto1);
+orden1.agregarProductos(producto2);
+orden1.mostrarOrden();
+let orden2 = new Orden();
+let producto3 = new Producto("camisa", 100);
+let producto4 = new Producto("zapatos", 250);
+orden2.agregarProductos(producto3);
+orden2.agregarProductos(producto1);
+orden2.agregarProductos(producto4);
+orden2.mostrarOrden();
